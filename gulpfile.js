e1a3eb4eed269eb2593ff6ab0cbf4c28f52589c5
Kai Curry <kai@sundaysenergy.com>
2015-03-25 19:01:15 -0400

    change url

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var appInfo = require('./package.json')
 // Create global variable SITE_ID based on name property.
 global.SITE_ID = appInfo.name
 global.DATA_URL = appInfo.homepage+'/index.json'
+
+console.log(global.DATA_URL);
+
 // Require coffeescript
 require('coffee-script/register');
 // Require cape-gulp that contains common tasks.
